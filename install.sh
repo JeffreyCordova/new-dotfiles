6c4779f40cf79462b6476421cd2d1a66b28603a4
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-01-02 21:37:40 -0800

    install.sh and deps.lst changes

--- install.sh
+++ install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
-#     _            __        ____          __               
-#    (_)___  _____/ /_____ _/ / /     ____/ /__  ____  _____
-#   / / __ \/ ___/ __/ __ `/ / /_____/ __  / _ \/ __ \/ ___/
-#  / / / / (__  ) /_/ /_/ / / /_____/ /_/ /  __/ /_/ (__  ) 
-# /_/_/ /_/____/\__/\__,_/_/_/      \__,_/\___/ .___/____/  
-#                                            /_/            
+#     _            __        ____
+#    (_)___  _____/ /_____ _/ / /
+#   / / __ \/ ___/ __/ __ `/ / / 
+#  / / / / (__  ) /_/ /_/ / / /  
+# /_/_/ /_/____/\__/\__,_/_/_/   
+#                                
 
-cmd="pacaur -S"
+cmd="trizen -S"
 $cmd $(cat deps.lst)
 
 # stow
-PKGS=(bspwm compton dunst git gtk julia npm nvim polybar profile resources \
+PKGS=(bspwm dunst git gtk julia npm nvim polybar profile resources \
     rofi scripts sxhkd templates termite tmux vscode xorg xwinmosaic zsh)
 
 for pkg in PKGS; do
-    stow pkg
+    stow $pkg
 done
 
-sudo npm install -g neovim
+#sudo npm install -g neovim
