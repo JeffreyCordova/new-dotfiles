f659f847de8653ab3adc616b1c74b60c668f4599
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-01-03 17:38:38 -0800

    corrected sed statement

--- install/chroot.sh
+++ install/chroot.sh
@@ -2,7 +2,7 @@
 
 ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 hwclock --systohc
-sed -i '/en_US.UTF-8/ s/^#//' /etc/locale.gen
+sed -i '0,/en_US.UTF-8/! {/en_US.UTF-8/ s/^#//}' /etc/locale.gen
 locale-gen
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
