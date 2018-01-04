d7aed340adbbb613a96edc0575314c57f8fe0b15
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-01-03 23:17:15 -0800

    minor changes

--- install.sh
+++ install.sh
@@ -37,3 +37,12 @@ gem install neovim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/install.sh)"
 rm -rf coreutils-8.2
+
+FONTS=( Bold Light Medium )
+cd /usr/share/fonts/OTF
+for font in "${FONTS[@]}"; do
+    curl -O https://raw.githubusercontent.com/ryanoasis/nerd-fonts/patched-fonts/\
+        Hermit/$font/complete/Hurmit\ $font\ Nerd\ Font\ Complete.otf
+    curl -O https://raw.githubusercontent.com/ryanoasis/nerd-fonts/patched-fonts/\
+        Hermit/$font/complete/Hurmit\ $font\ Nerd\ Font\ Complete\ Mono.otf
+done
