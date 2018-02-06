3ba38e91ad1433f86e97d45a4266bdb2b42d72ac
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-02-05 21:53:40 -0800

    added trizen/

--- install.sh
+++ install.sh
@@ -22,14 +22,14 @@ rm -rf trizen
 gpg --recv-keys 1EB2638FF56C0C53
 gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
 
-sudo pacman -S --noconfirm $(cat deps/official.lst)
+sudo pacman -S --needed --noconfirm $(cat deps/official.lst)
 trizen -S --needed --noconfirm $(cat deps/aur.lst)
 
 chsh -s /usr/bin/zsh
 
 # stow
-PKGS=( bspwm dunst git gtk julia npm nvim polybar profile resources \
-    rofi scripts sxhkd templates termite tmux vscode xorg xwinmosaic zsh )
+PKGS=( bspwm dunst git gtk julia npm nvim polybar profile resources rofi \
+    scripts sxhkd templates termite tmux trizen vscode xorg xwinmosaic zsh )
 
 for pkg in "${PKGS[@]}"; do
     stow $pkg
