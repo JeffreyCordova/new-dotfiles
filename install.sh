e2dddb855624f1bed8d769c5afcebbd2355942d7
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-03-14 04:17:28 -0700

    refactored bspwm/

--- install.sh
+++ install.sh
@@ -9,41 +9,43 @@
 platform=$1
 opt=$2
 
-sudo refind-install
-sudo cp -r refind/EFI/* /boot/
+BASE_PKGS=( git gtk julia npm nvim profile resources rofi scripts \
+    templates termite tmux vscode zsh )
+BSPWM_PKGS=( bspwm polybar xwinmosaic )
+XMONAD_PKGS=( xmonad )
 
-sudo pacman -S --noconfirm reflector
-sudo reflector --verbose \
-               --protocol https \
-               --latest 200 \
-               --sort rate \
-               --save /etc/pacman.d/mirrorlist
-sudo pacman -Syy
+function refind_install() {
+    sudo refind-install
+    sudo cp -r refind/EFI/* /boot/
+}
 
-git clone https://aur.archlinux.org/trizen
-cd trizen
-makepkg -sri
-cd ..
-rm -rf trizen
+function reflector() {
+    sudo pacman -S --noconfirm reflector
+    sudo reflector --verbose \
+                   --protocol https \
+                   --latest 200 \
+                   --sort rate \
+                   --save /etc/pacman.d/mirrorlist
+    sudo pacman -Syy
+}
 
-gpg --recv-keys 1EB2638FF56C0C53                            # pacaur
-gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A    # aurutils
+function trizen_install() {
+    git clone https://aur.archlinux.org/trizen
+    cd trizen
+    makepkg -sri
+    cd ..
+    rm -rf trizen
+}
 
+function add_pgp_keys() {
+    gpg --recv-keys 1EB2638FF56C0C53                            # pacaur
+    gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A    # aurutils
+}
+
+function base_install() {
+
+}
 trizen -S --needed --noconfirm $(cat deps/base.deps)
-
-if [ "$platform" == "desktop" ]; then
-    trizen -S --needed --noconfirm $(cat deps/desktop.deps)
-elif [ "$platform" == "laptop" ]; then
-    trizen -S --needed --noconfirm $(cat deps/laptop.deps)
-fi
-
-if [ "$opt" == "bspwm" ]; then
-    trizen -S --needed --noconfirm $(cat deps/bspwm.deps)
-elif [ "$opt" == "xmonad" ]; then
-    trizen -S --needed --noconfirm $(cat deps/xmonad.deps)
-fi
-
-chsh -s /usr/bin/zsh
 
 PKGS=( bspwm compton dunst git gtk julia npm nvim polybar profile resources rofi \
     scripts sxhkd templates termite tmux vscode xorg xwinmosaic zsh )
