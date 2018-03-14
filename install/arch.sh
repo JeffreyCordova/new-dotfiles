48dcfe363c046185339e3f2bf27b881a2e803ce2
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-03-14 09:21:00 -0700

    refactored install scripts

--- install/arch.sh
+++ install/arch.sh
@@ -1,38 +1,55 @@
 #!/bin/sh
 
-mount /dev/sda6 /mnt
-mount /dev/sda2 /mnt/boot
+function mount_partitions() {
+    mount /dev/sda6 /mnt
+    if [ ! -d "/mnt/boot" ]; then
+        mkdir -p /mnt/boot
+    fi
+    mount /dev/sda2 /mnt/boot
+}
 
-cd /mnt/boot
-rm refind_linux.conf vmlinuz-linux intel-ucode.img initramfs-linux.img initramfs-linux-fallback.img
-rm -rf loader EFI/refind EFI/systemd
-cd ..
-umount /mnt/boot
-rm -rf *
-cd ~
-umount /mnt
+function clean_partitions() {
+    cd /mnt/boot
+    rm refind_linux.conf vmlinuz-linux intel-ucode.img initramfs-linux.img initramfs-linux-fallback.img
+    rm -rf loader EFI/refind EFI/systemd
+    cd ..
+    umount /mnt/boot
+    rm -rf *
+    cd ~
+    umount /mnt
+}
 
-mkfs.ext4 /dev/sda6
-mkswap /dev/sda7
-swapon /dev/sda7
+function format_partitions() {
+    mkfs.ext4 /dev/sda6
+    mkswap /dev/sda7
+    swapon /dev/sda7
+}
 
-mount /dev/sda6 /mnt
-mkdir -p /mnt/boot
-mount /dev/sda2 /mnt/boot
+function run_reflector() {
+    pacman -Syy --noconfirm reflector
+    reflector --verbose \
+              --protocol https \
+              --latest 200 \
+              --sort rate \
+              --save /etc/pacman.d/mirrorlist
+    pacman -Syy
+}
 
-pacman -Syy --noconfirm reflector
-reflector --verbose \
-          --protocol https \
-          --latest 200 \
-          --sort rate \
-          --save /etc/pacman.d/mirrorlist
-pacman -Syy
+function arch_install() {
+    pacstrap /mnt base{,-devel}
+    genfstab -U -p /mnt >> /mnt/etc/fstab
+}
 
-pacstrap /mnt base{,-devel}
-genfstab -U -p /mnt >> /mnt/etc/fstab
+mount_partitions
+clean_partitions
+mount_partitions
+format_partitions
 
-curl -O https://raw.githubusercontent.com/JeffreyCordova/dotfiles/laptop/install/chroot.sh
-chmod +x chroot.sh
+run_reflector
+arch_install
+
+#curl -O https://raw.githubusercontent.com/JeffreyCordova/dotfiles/laptop/install/chroot.sh
+#chmod +x chroot.sh
 
 cp chroot.sh /mnt/
 arch-chroot /mnt ./chroot.sh
--- install/chroot.sh
+++ install/chroot.sh
@@ -1,34 +1,60 @@
 #!/bin/sh
 
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
-sed -i '0,/en_US.UTF-8/! {/en_US.UTF-8/ s/^#//}' /etc/locale.gen
-locale-gen
-echo LANG=en_US.UTF-8 > /etc/locale.conf
-hwclock --systohc
+function locale_config() {
+    ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
 
-echo c137 > /etc/hostname
-echo "127.0.1.1\tc137.localdomain\tc137" >> /etc/hosts
+    sed -i '0,/en_US.UTF-8/! {/en_US.UTF-8/ s/^#//}' /etc/locale.gen
+    locale-gen
 
-pacman -S --noconfirm refind-efi intel-ucode wpa_supplicant dialog
-bootctl --path=/boot install
-curl -O https://raw.githubusercontent.com/JeffreyCordova/dotfiles/laptop/refind/loader/loader.conf
-curl -O https://raw.githubusercontent.com/JeffreyCordova/dotfiles/laptop/refind/loader/entries/arch.conf
-mv loader.conf /boot/loader/
-mv arch.conf /boot/loader/entries/
+    echo LANG=en_US.UTF-8 > /etc/locale.conf
 
-sed -i '/Color/s/^#//' /etc/pacman.conf
-sed -i '/\[multilib\]/,+1 s/^#//' /etc/pacman.conf
-pacman -Syy
+    hwclock --systohc
+}
 
-pacman -S --noconfirm git
+function host_config() {
+    echo c137 > /etc/hostname
+    echo "127.0.1.1\tc137.localdomain\tc137" >> /etc/hosts
+}
 
-passwd
+function bootloader_install() {
+    pacman -S --noconfirm git refind-efi intel-ucode wpa_supplicant dialog
 
-useradd -m -G wheel -s /bin/bash jeff
-passwd jeff
+    bootctl --path=/boot install
 
-visudo
+    curl -O https://raw.githubusercontent.com/JeffreyCordova/dotfiles/laptop/refind/loader/loader.conf
+    curl -O https://raw.githubusercontent.com/JeffreyCordova/dotfiles/laptop/refind/loader/entries/arch.conf
 
-echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+    mv loader.conf /boot/loader/
+    mv arch.conf /boot/loader/entries/
+}
+
+function pacman_config() {
+    sed -i '/Color/s/^#//' /etc/pacman.conf
+    sed -i '/\[multilib\]/,+1 s/^#//' /etc/pacman.conf
+
+    pacman -Syy
+}
+
+function user_password_config() {
+    passwd
+
+    useradd -m -G wheel -s /bin/bash jeff
+    passwd jeff
+
+    visudo
+}
+
+function disable_beep() {
+    echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+}
+
+locale_config
+host_config
+
+bootloader_install
+pacman_config
+user_password_config
+
+disable_beep
 
 exit
