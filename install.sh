7ad5fcc85754d4e40a1b6da86d994f1335cd20c5
Jeffrey Cordova <jeffcordova@protonmail.com>
2018-03-14 03:50:23 -0700

    refactored deps/

--- install.sh
+++ install.sh
@@ -6,14 +6,18 @@
 # /_/_/ /_/____/\__/\__,_/_/_/   
 #                                
 
+platform=$1
+opt=$2
+
 sudo refind-install
 sudo cp -r refind/EFI/* /boot/
 
 sudo pacman -S --noconfirm reflector
-sudo reflector --verbose --protocol https \
-                         -l 200 \
-                         --sort rate \
-                         --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose \
+               --protocol https \
+               --latest 200 \
+               --sort rate \
+               --save /etc/pacman.d/mirrorlist
 sudo pacman -Syy
 
 git clone https://aur.archlinux.org/trizen
@@ -22,15 +26,25 @@ makepkg -sri
 cd ..
 rm -rf trizen
 
-gpg --recv-keys 1EB2638FF56C0C53
-gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A
+gpg --recv-keys 1EB2638FF56C0C53                            # pacaur
+gpg --recv-keys DBE7D3DD8C81D58D0A13D0E76BC26A17B9B7018A    # aurutils
 
-sudo pacman -S --noconfirm $(cat deps/official.lst)
-trizen -S --needed --noconfirm $(cat deps/aur.lst)
+trizen -S --needed --noconfirm $(cat deps/base.deps)
+
+if [ "$platform" == "desktop" ]; then
+    trizen -S --needed --noconfirm $(cat deps/desktop.deps)
+elif [ "$platform" == "laptop" ]; then
+    trizen -S --needed --noconfirm $(cat deps/laptop.deps)
+fi
+
+if [ "$opt" == "bspwm" ]; then
+    trizen -S --needed --noconfirm $(cat deps/bspwm.deps)
+elif [ "$opt" == "xmonad" ]; then
+    trizen -S --needed --noconfirm $(cat deps/xmonad.deps)
+fi
 
 chsh -s /usr/bin/zsh
 
-# stow
 PKGS=( bspwm compton dunst git gtk julia npm nvim polybar profile resources rofi \
     scripts sxhkd templates termite tmux vscode xorg xwinmosaic zsh )
 
@@ -39,6 +53,3 @@ for pkg in "${PKGS[@]}"; do
 done
 
 sudo npm install -g neovim
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/illinoisjackson/even-better-ls/master/install.sh)"
-rm -rf coreutils-8.2
